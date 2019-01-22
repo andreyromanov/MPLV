37dae9e0abd7b12b4fa87708a78290d203dac14b
andreyromanov <andreygorogogo@gmail.com>
2019-01-23 00:16:19 +0200

    Vue form. Users table.

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,12 +1,101 @@
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
-                <div class="card card-default">
-                    <div class="card-header">Users Component</div>
+        <div class="row pt-3">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Users Table</h3>
 
-                    <div class="card-body">
-                        I'm an example component.
+                        <div class="card-tools">
+                            <button class="btn btn-success" data-toggle="modal" data-target="#addNew">Add user <i class="fas fa-user-plus"></i></button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover">
+                            <tbody>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Type</th>
+                                    <th>Modify</th>
+                                </tr>
+                                <tr>
+                                    <td>183</td>
+                                    <td>John Doe</td>
+                                    <td>11-7-2014</td>
+                                    <td><span class="tag tag-success">Approved</span></td>
+                                    <td>
+                                        <a href=""><i class="fa fa-edit"></i></a>
+                                        /
+                                        <a href=""><i class="fa fa-trash text-danger"></i></a>
+                                    </td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNew" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addNew">Add New</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input v-model="form.name" type="text" name="name" class="form-control" :class="{ 'is-invalid': form.errors.has('name') }"
+                                placeholder="Name">
+                            <has-error :form="form" field="name"></has-error>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input v-model="form.email" type="email" name="email" class="form-control" :class="{ 'is-invalid': form.errors.has('email') }"
+                                placeholder="Email">
+                            <has-error :form="form" field="email"></has-error>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Bio</label>
+                            <textarea v-model="form.bio" name="bio" id="bio" class="form-control" :class="{ 'is-invalid': form.errors.has('bio') }"
+                                placeholder="Bio (Optional)"></textarea>
+                            <has-error :form="form" field="bio"></has-error>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Type</label>
+                            <select v-model="form.type" name="type" id="type" class="form-control" :class="{ 'is-invalid': form.errors.has('type') }">
+                                <option value="">Select role</option>
+                                <option value="admin">Admin</option>
+                                <option value="user">User</option>
+                                <option value="author">Author</option>
+                            </select>
+                            <has-error :form="form" field="type"></has-error>
+                        </div>
+
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input v-model="form.password" type="password" name="password" id="password" class="form-control" 
+                            :class="{ 'is-invalid': form.errors.has('password') }" placeholder="Password">
+                            <has-error :form="form" field="password"></has-error>
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Create</button>
                     </div>
                 </div>
             </div>
@@ -16,8 +105,23 @@
 
 <script>
     export default {
+
+        data() {
+            return {
+                form: new Form({
+                    name: '',
+                    email: '',
+                    password: '',
+                    type: '',
+                    bio: '',
+                    photo: ''
+                })
+            }
+        },
+
         mounted() {
             console.log('Component mounted.')
         }
     }
+
 </script>
