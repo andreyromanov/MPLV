380cf251f1c74b49efb527034cf30838e03c06d2
andreyromanov <andreygorogogo@gmail.com>
2019-02-13 08:52:39 +0200

    Update phptp in profile

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -9,7 +9,7 @@
                         <h5 class="widget-user-desc">Web Designer</h5>
                     </div>
                     <div class="widget-user-image">
-                        <img class="img-circle" src="" alt="User Avatar">
+                        <img class="img-circle" :src="getProfilePhoto()" alt="User Avatar">
                     </div>
                     <div class="card-footer">
                         <div class="row">
@@ -245,6 +245,9 @@
         },
 
         methods: {
+            getProfilePhoto(){
+                return "img/profile/" + this.form.photo;
+            },
 
             updateInfo() {
                this.$Progress.start();
