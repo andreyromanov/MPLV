d396bbedf2660984a852dcd9e63469f46094ccad
andreyromanov <andreygorogogo@gmail.com>
2019-02-16 17:32:47 +0200

    Print page

--- resources/js/app.js
+++ resources/js/app.js
@@ -118,6 +118,10 @@ const app = new Vue({
       methods: {
         searchit: _.debounce(() => {
           Fire.$emit('searching');
-        }, 1000)
+        }, 1000),
+
+        printme() {
+          window.print();
+        }
           }
 });
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/invoice', function () {
+    return view('invoice');
+});
 
 Auth::routes();
 
